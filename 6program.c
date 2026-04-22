fd7fd27769f3128a1033e70622695cb5d128b476
Hardeep Singh <hardeepfitness143@gmail.com>
2026-04-22 15:20:08 +0530

    WAP to find length of string

---
+++ 6program.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[] = "Hardeep";
+
+    int length = strlen(str);
+    printf("Length of is :  %d", length);
+
+    return 0;
+}
