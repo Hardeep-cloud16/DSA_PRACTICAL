81489f0774ba69338d3b60837102f59365e807dc
Hardeep Singh <hardeepfitness143@gmail.com>
2026-04-22 09:05:33 +0530

    WAP TO TRAVERSE ON ARRAY

---
+++ 1program.c
@@ -0,0 +1,15 @@
+// WAP TO TRAVERSE ON ARRAY
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(int);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
